72241a768bf2faeff2c39263b1ab1b1d520b8aad
Benjamin <benjaminhomer@gmail.com>
2014-03-08 13:23:26 -0700

    Angular installed

---
+++ app/scripts/app.js
@@ -0,0 +1,5 @@
+'use strict';
+
+angular.module('spreadsheetStopwatchApp', []);
+
+mongoose.connect(mongodb://bhomer:de46dbaa3597b96364708d980b5281ec.mongolab.com:33429/spreadsheetstopwatch
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('spreadsheetStopwatchApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
