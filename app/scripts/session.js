2d54df2e299db69bf10a0276f2265eb596e2f8ba
Benjamin <benjaminhomer@gmail.com>
2014-03-11 19:37:27 -0600

    add session.js, student.js

---
+++ app/scripts/session.js
@@ -0,0 +1,12 @@
+//var mongoose = require('mongoose');
+//var Schema = mongoose.Schema;
+//
+//var Session = mongoose.model('Session', new mongoose.Schema({
+//        student: { type: Number, ref: 'Student' },
+//        materialCovered: String,
+//        duration: Number,
+//        date: { type: Date, default: Date.now },
+//        notes: String
+//));
+//
+//module.exports = Student;
---
+++ app/scripts/student.js
@@ -0,0 +1,15 @@
+//var mongoose = require('mongoose');
+//var Schema = mongoose.Schema;
+//
+//var Student = mongoose.model('Student', new mongoose.Schema({
+//        name: String,
+//        subjects: [],
+//        hourlyRate: String,
+//        contact: {
+//            email: String,
+//            phone: Number,
+//            Address: String
+//        }
+//));
+//
+//module.exports = Student;
